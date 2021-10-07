e7f4e587ab593d16016f801d45c073d62859aa4c
emrecanAy <codemrecan@gmail.com>
2021-10-08 00:19:39 +0300

    Odev1 tamam!

---
+++ src/com/patika/Odev1.java
@@ -0,0 +1,49 @@
+package com.patika;
+
+import java.util.Scanner;
+
+public class Odev1 {
+
+    public static void main(String[] args) {
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz : ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double ortalama = (toplam / 6);
+
+        boolean gectiMi = ortalama > 60;
+
+        System.out.println("Ortalamanız : " + ortalama);
+        System.out.println(gectiMi ? "Sınıfı Geçti!" : "Sınıfta Kaldı!");
+
+
+
+
+
+
+
+
+
+
+    }
+}
